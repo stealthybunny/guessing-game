1621a8891386fe2b3e667b9b71aaf7791048e5e4
Peter Zaichyk <zaichikpetr@gmail.com>
2022-08-16 21:43:00 +0300

    feat: finished

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,25 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
 
     }
 
     guess() {
+        this.mid = Math.ceil((Math.ceil(this.min) + Math.ceil(this.max)) / 2);
+        return this.mid;
 
     }
 
     lower() {
+        this.max = this.mid;
+
 
     }
 
     greater() {
-
+        this.min = this.mid;
     }
 }
 
